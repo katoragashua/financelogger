7c7094cba6534d58f613c445b5140db59aacc03c
Kator Agashua <katoragashua@outlook.com>
2022-08-15 07:39:18 +0100

    first commit

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var mario = 'mario';
+console.log(mario);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
---
+++ sandbox.ts
@@ -0,0 +1,6 @@
+const mario = 'mario'
+console.log(mario)
+
+const inputs = document.querySelectorAll("input") 
+
+console.log(inputs)
